f378b09d3e6fce30e613f7dd900dfb973427fa8e
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-05-20 21:35:04 +0930

    Sequelize_Activity_StarWarsApp

---
+++ app/routes/api-routes.js
@@ -0,0 +1,63 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var Character = require("../models/character.js");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Search for Specific Character (or all characters) then provides JSON
+  app.get("/api/:charcters?", function(req, res) {
+
+    // If the user provides a specific character in the URL...
+    if (req.params.characters) {
+
+      // Then display the JSON for ONLY that character.
+      // (Note how we're using the ORM here to run our searches)
+      Character.findone({
+        where: {
+          routeName: req.params.characters
+      }
+    }).then ((character) => {
+        res.json(character);
+    })
+    }
+
+    // Otherwise...
+    else {
+      // Otherwise display the data for all of the characters.
+      // (Note how we're using the ORM here to run our searches)
+      Character.findAll().then ((characters) => {
+        res.json(characters);
+      });
+    }
+
+  });
+
+  // If a user sends data to add a new character...
+  app.post("/api/new", function(req, res) {
+
+    // Take the request...
+    var character = req.body;
+    var routeName = character.name.replace(/\s+/g, "").toLowerCase();
+
+
+    // Then send it to the ORM to "save" into the DB.
+    //copying character object with ... (spread operator) we are creating new character. 
+    Character.create({
+      routeName: routeName,
+      name: character.name,
+      role: character.role,
+      age: character.age,
+      forcePoints: character.forcePoints
+    }).then((status) => {
+      console.log(status)
+      res.status(204).end()
+    })
+  })
+};
